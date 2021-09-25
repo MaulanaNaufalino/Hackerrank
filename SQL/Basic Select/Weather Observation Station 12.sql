904b98b1d4bf69d980a28cbcf3282f8d6a83deab
Maulana Naufalino <86004659+MaulanaNaufalino@users.noreply.github.com>
2021-09-25 15:54:29 +0700

    Create Weather Observation Station 12.sql

---
+++ SQL/Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,6 @@
+select distinct CITY
+from STATION
+where 
+(not (CITY like "A%" or CITY like "E%" or CITY like "I%" or CITY like "O%" or CITY like "U%"))
+and
+(not (CITY like "%A" or CITY like "%E" or CITY like "%I" or CITY like "%O" or CITY like "%U"));
